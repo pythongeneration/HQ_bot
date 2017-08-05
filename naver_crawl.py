67ad3a37247c7ba2aa9b0fa83538cf84c6ad1500
pythongeneration <seungsiklearning@gmail.com>
2017-08-05 17:35:58 +0900

    Update and rename naver_crawl to naver_crawl.py

---
+++ naver_crawl.py
@@ -0,0 +1,24 @@
+def crawling_naver():
+
+    import requests
+
+    from bs4 import BeautifulSoup as bsoup
+
+    html = requests.get('http://news.naver.com').text
+
+    soup = bsoup(html, 'html.parser')
+
+    # print(soup)
+    D = {}
+    for i, tag in enumerate(soup.select('#ranking_105 a')):
+        i += 1
+        tag_noline = tag.text.split()
+        tag_html = 'http://news.naver.com' + tag['href']
+        # print(i,  tag['title']+tag_html)
+        D[tag['title']] = tag_html
+    return print(D)
+
+
+if __name__ == "__main__":
+
+    crawling_naver()
